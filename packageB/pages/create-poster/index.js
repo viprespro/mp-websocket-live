52ccbaa65a40cd5a085d36a1f007ae96a56edbf0
Aspire01 <18253137881@163.com>
2020-06-19 10:40:17 +0800

    websocket 测试ok

--- packageB/pages/create-poster/index.js
+++ packageB/pages/create-poster/index.js
@@ -1,5 +1,7 @@
 const util = require('../../../utils/util.js')
-import { $api } from '../../../common/utils.js'
+import {
+  $api
+} from '../../../common/utils.js'
 import Poster from '../../../miniprogram_dist/poster/poster';
 const api = require('../../../utils/api-tp.js')
 const Config = require('../../../config.js')
@@ -11,144 +13,141 @@ const posterConfig = {
     backgroundColor: '#fff',
     debug: false,
     pixelRatio: 1,
-    blocks: [
-      {
-        width: 690,
-        height: 708,
-        x: 30,
-        y: 183,
-        borderWidth: 2,
-        borderColor: '#f0c2a0',
-        borderRadius: 20,
-      },
-      {
-        width: 634,
-        height: 74,
-        x: 59,
-        y: 770,
-        backgroundColor: '#fff',
-        opacity: 0.5,
-        zIndex: 100,
-      },
+    blocks: [{
+      width: 690,
+      height: 708,
+      x: 30,
+      y: 183,
+      borderWidth: 2,
+      borderColor: '#f0c2a0',
+      borderRadius: 20,
+    },
+    {
+      width: 634,
+      height: 74,
+      x: 59,
+      y: 770,
+      backgroundColor: '#fff',
+      opacity: 0.5,
+      zIndex: 100,
+    },
     ],
-    texts: [
-      {
-        x: 113,
-        y: 61,
-        baseLine: 'middle',
-        text: 'Ares',
-        fontSize: 28,
-        color: '#8d8d8d',
-      },
-      {
-        x: 30,
-        y: 113,
-        baseLine: 'top',
-        text: '直播的时间',
-        fontSize: 36,
-        color: '#333',
-      },
-      {
-        x: 92,
-        y: 810,
-        fontSize: 38,
-        baseLine: 'middle',
-        text: '直播间的标题',
-        width: 570,
-        lineNum: 1,
-        color: '#000',
-        zIndex: 200,
-      },
-      // {
-      //     x: 59,
-      //     y: 895,
-      //     baseLine: 'middle',
-      //     text: [
-      //         {
-      //             text: '2人拼',
-      //             fontSize: 28,
-      //             color: '#ec1731',
-      //         },
-      //         {
-      //             text: '¥99',
-      //             fontSize: 36,
-      //             color: '#ec1731',
-      //             marginLeft: 30,
-      //         }
-      //     ]
-      // },
-      // {
-      //     x: 522,
-      //     y: 895,
-      //     baseLine: 'middle',
-      //     text: '已拼2件',
-      //     fontSize: 28,
-      //     color: '#929292',
-      // },
-      // {
-      //     x: 59,
-      //     y: 945,
-      //     baseLine: 'middle',
-      //     text: [
-      //         {
-      //             text: '商家发货&售后',
-      //             fontSize: 28,
-      //             color: '#929292',
-      //         },
-      //         {
-      //             text: '七天退货',
-      //             fontSize: 28,
-      //             color: '#929292',
-      //             marginLeft: 50,
-      //         },
-      //         {
-      //             text: '运费险',
-      //             fontSize: 28,
-      //             color: '#929292',
-      //             marginLeft: 50,
-      //         },
-      //     ]
-      // },
-      {
-        x: 360,
-        y: 1065,
-        baseLine: 'top',
-        text: '长按识别小程序码',
-        fontSize: 38,
-        color: '#080808',
-      },
-      {
-        x: 360,
-        y: 1123,
-        baseLine: 'top',
-        text: '超值好货一起拼',
-        fontSize: 28,
-        color: '#929292',
-      },
+    texts: [{
+      x: 113,
+      y: 61,
+      baseLine: 'middle',
+      text: 'Ares',
+      fontSize: 28,
+      color: '#8d8d8d',
+    },
+    {
+      x: 30,
+      y: 113,
+      baseLine: 'top',
+      text: '直播的时间',
+      fontSize: 36,
+      color: '#333',
+    },
+    {
+      x: 92,
+      y: 810,
+      fontSize: 38,
+      baseLine: 'middle',
+      text: '直播间的标题',
+      width: 570,
+      lineNum: 1,
+      color: '#000',
+      zIndex: 200,
+    },
+    // {
+    //     x: 59,
+    //     y: 895,
+    //     baseLine: 'middle',
+    //     text: [
+    //         {
+    //             text: '2人拼',
+    //             fontSize: 28,
+    //             color: '#ec1731',
+    //         },
+    //         {
+    //             text: '¥99',
+    //             fontSize: 36,
+    //             color: '#ec1731',
+    //             marginLeft: 30,
+    //         }
+    //     ]
+    // },
+    // {
+    //     x: 522,
+    //     y: 895,
+    //     baseLine: 'middle',
+    //     text: '已拼2件',
+    //     fontSize: 28,
+    //     color: '#929292',
+    // },
+    // {
+    //     x: 59,
+    //     y: 945,
+    //     baseLine: 'middle',
+    //     text: [
+    //         {
+    //             text: '商家发货&售后',
+    //             fontSize: 28,
+    //             color: '#929292',
+    //         },
+    //         {
+    //             text: '七天退货',
+    //             fontSize: 28,
+    //             color: '#929292',
+    //             marginLeft: 50,
+    //         },
+    //         {
+    //             text: '运费险',
+    //             fontSize: 28,
+    //             color: '#929292',
+    //             marginLeft: 50,
+    //         },
+    //     ]
+    // },
+    {
+      x: 360,
+      y: 1065,
+      baseLine: 'top',
+      text: '长按识别小程序码',
+      fontSize: 38,
+      color: '#080808',
+    },
+    {
+      x: 360,
+      y: 1123,
+      baseLine: 'top',
+      text: '超值好货一起拼',
+      fontSize: 28,
+      color: '#929292',
+    },
     ],
-    images: [
-      {
-        width: 62,
-        height: 62,
-        x: 30,
-        y: 30,
-        borderRadius: 62,
-        url: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1587710287566&di=a06cf84c8d5b53dc4aef41149d8ff8b1&imgtype=0&src=http%3A%2F%2Fa3.att.hudong.com%2F14%2F75%2F01300000164186121366756803686.jpg',
-      },
-      {
-        width: 634,
-        height: 634,
-        x: 59,
-        y: 210,
-        url: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1587710287566&di=a06cf84c8d5b53dc4aef41149d8ff8b1&imgtype=0&src=http%3A%2F%2Fa3.att.hudong.com%2F14%2F75%2F01300000164186121366756803686.jpg',
-      },
-      {
-        width: 220,
-        height: 220,
-        x: 92,
-        y: 1020,
-        url: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1587710287566&di=a06cf84c8d5b53dc4aef41149d8ff8b1&imgtype=0&src=http%3A%2F%2Fa3.att.hudong.com%2F14%2F75%2F01300000164186121366756803686.jpg',
-      },
+    images: [{
+      width: 62,
+      height: 62,
+      x: 30,
+      y: 30,
+      borderRadius: 62,
+      url: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1587710287566&di=a06cf84c8d5b53dc4aef41149d8ff8b1&imgtype=0&src=http%3A%2F%2Fa3.att.hudong.com%2F14%2F75%2F01300000164186121366756803686.jpg',
+    },
+    {
+      width: 634,
+      height: 634,
+      x: 59,
+      y: 210,
+      url: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1587710287566&di=a06cf84c8d5b53dc4aef41149d8ff8b1&imgtype=0&src=http%3A%2F%2Fa3.att.hudong.com%2F14%2F75%2F01300000164186121366756803686.jpg',
+    },
+    {
+      width: 220,
+      height: 220,
+      x: 92,
+      y: 1020,
+      url: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1587710287566&di=a06cf84c8d5b53dc4aef41149d8ff8b1&imgtype=0&src=http%3A%2F%2Fa3.att.hudong.com%2F14%2F75%2F01300000164186121366756803686.jpg',
+    },
     ]
 
   },
@@ -163,14 +162,14 @@ Page({
     title: '', // 直播间标题
     cover: '', // 直播间封面
     time: '', // 直播时间
-    avatar:'',
+    avatar: '',
     nickname: '',
     number: '', // 直播的房间好
     path: '', // 封面的网络地址
-    qrCode: '',// 小程序二维码
+    qrCode: '', // 小程序二维码
     startDate: '',
     startTime: '',
-    endDate: '', 
+    endDate: '',
     endTime: ''
   },
 
@@ -179,8 +178,10 @@ Page({
    */
   onLoad: function (opts) {
     console.log(opts)
-    if(opts.number) {
-      this.setData({ number: opts.number })
+    if (opts.number) {
+      this.setData({
+        number: opts.number
+      })
     }
 
     // 若是自定义的时间
@@ -191,9 +192,13 @@ Page({
   },
 
   bindTimeChange: function (e) {
-    let { index } = e.currentTarget.dataset
-    let { value } = e.detail; 
-    if(index == 1) {
+    let {
+      index
+    } = e.currentTarget.dataset
+    let {
+      value
+    } = e.detail;
+    if (index == 1) {
       this.setData({
         startTime: value
       })
@@ -206,9 +211,13 @@ Page({
   },
 
   bindDateChange: function (e) {
-    let { index } = e.currentTarget.dataset
-    let { value } = e.detail; 
-    if(index == 0) {
+    let {
+      index
+    } = e.currentTarget.dataset
+    let {
+      value
+    } = e.detail;
+    if (index == 0) {
       this.setData({
         startDate: value
       })
@@ -226,7 +235,10 @@ Page({
       title: '上传中...',
       mask: false
     })
-    let { cover, number } = this.data
+    let {
+      cover,
+      number
+    } = this.data
     wx.uploadFile({
       url: Config.HTTP_REQUEST_URL + '/wxsmall/live/getWxCode',
       filePath: cover,
@@ -260,20 +272,30 @@ Page({
       },
       success: res => {
         // console.log(res)
-        let { avatar, nickname } = res.data
-        this.setData({ avatar, nickname})
+        let {
+          avatar,
+          nickname
+        } = res.data
+        this.setData({
+          avatar,
+          nickname
+        })
       }
     })
   },
 
   // 绑定直播时间
   bindTime(e) {
-    this.setData({ time: e.detail.value })
+    this.setData({
+      time: e.detail.value
+    })
   },
 
   // 绑定直播间的标题
   bindTitle(e) {
-    this.setData({ title: e.detail.value })
+    this.setData({
+      title: e.detail.value
+    })
   },
 
   // 上传图片
@@ -296,10 +318,21 @@ Page({
 
   // 生成海报
   onCreateOtherPoster() {
-    let { title, cover, avatar, nickname, path, qrCode, startDate, startTime, endDate, endTime} = this.data
-    if(!title) {
+    let {
+      title,
+      cover,
+      avatar,
+      nickname,
+      path,
+      qrCode,
+      startDate,
+      startTime,
+      endDate,
+      endTime
+    } = this.data
+    if (!title) {
       return $api.msg('直播标题不能为空')
-    }else if(!cover) {
+    } else if (!cover) {
       return $api.msg('直播封面不能为空')
     } else if (!startDate || !startTime || !endDate || !endTime) {
       return $api.msg('直播时间请填写完整')
@@ -308,37 +341,38 @@ Page({
     let config = posterConfig.jdConfig
     let texts = config.texts
     let imgs = config.images
-    imgs[0].url = avatar  //  设置头像
-    imgs[1].url = path  //  设置头像
+    imgs[0].url = avatar //  设置头像
+    imgs[1].url = path //  设置头像
     imgs[2].url = qrCode
     texts[0].text = nickname // 设置昵称
-    texts[1].text = '直播时间：' + this.foo(startDate) + ' ' + startTime + '至' +  this.foo(endDate) + ' ' + endTime  // 设置直播时间
+    texts[1].text = '直播时间：' + this.getCusTime(startDate) + ' ' + startTime + '至' + this.getCusTime(endDate) + ' ' + endTime // 设置直播时间
     texts[2].text = title // 设置直播标题
 
-    this.setData({ posterConfig: config }, () => {
-      Poster.create();    // 入参：true为抹掉重新生成 
+    this.setData({
+      posterConfig: config
+    }, () => {
+      Poster.create(); // 入参：true为抹掉重新生成 
     });
   },
 
   // 2020-04-28 转为4-28
-  foo(argus) {
-    let index = argus.indexOf('-')
-    let ret1 = argus.slice(index + 1)
-    let index2 = ret1.indexOf('-')
-    let ret2 = ret1.slice(0, index2)
-    let ret3 = ret1.slice(index2)
-    if (ret2 < 10) {
-      ret2 = ret2.slice(1)
+  getCusTime(str) {
+    let index = str.indexOf('-') // -的index
+    let remain = str.slice(index + 1)
+    let arr = remain.split('-')
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] < 10 && arr[i].length == 2) {
+        arr[i] = arr[i].slice(1)
+      }
     }
-    if (ret3 < 10) {
-      ret3 = ret3.slice(1)
-    }
-    ret3 = ret3.slice(1)
-    return ret2 + '月' + ret3
+    const [month, day] = arr
+    return month + '月' + day + '日'
   },
 
   onPosterSuccess(e) {
-    const { detail } = e;
+    const {
+      detail
+    } = e;
     wx.previewImage({
       current: detail,
       urls: [detail]
